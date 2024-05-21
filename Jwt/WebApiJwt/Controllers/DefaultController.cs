88620ac14765bdcec5e2f333cd37a78825cb8b7e
cengizhulya <hulyacengiz0@hotmail.com>
2024-05-21 15:50:48 +0300

    Add a method to create JWT tokens with claims

---
+++ Jwt/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetToken()
+        {
+            return Ok(new Token().CreateToken());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetClaimToken()
+        {
+            return Ok(new Token().CreateClaimToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult test1()
+        {
+            return Ok("test");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult test2()
+        {
+            return Ok("Succeded to enter with the CLAIM TOKEN.");
+        }
+    }
+}
